5201d0a749b4edc2de7ab3c3a6fa64a5e8b69ff7
gaowuqiang <291221561@qq.com>
2020-03-24 16:53:35 +0800

    '第一次修改'

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(123);
+class Games {
+    constructor() {
+        this.init();
+    }
+    init() {
+        let a = document.createElement(`div`);
+        a.innerText = `hello word`;
+        document.body.appendChild(a);
+    }
+}
+window.onload = () => {
+    let game = new Games();
+}
